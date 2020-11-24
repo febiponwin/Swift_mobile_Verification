2b2f8f776f6f4105f6ae96ad12c8af23454276cd
Febi Ponwin <41968911+febiponwin@users.noreply.github.com>
2020-11-24 23:15:28 +0530

    added code for edit operation

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def addrec():
          with sql.connect("database.db") as con:
             cur = con.cursor()
             
-            cur.execute("INSERT INTO user_info (cws_id,phone_no,phone_model,created_time,id) VALUES (?,?,?,?,?)", (cws,phone_no,phone_model,created_time,ids) )
+            cur.execute("INSERT INTO user_info (cws_id,phone_no,phone_model,created_time,id,updated_time) VALUES (?,?,?,?,?,?)", (cws,phone_no,phone_model,created_time,ids,created_time) )
             
             con.commit()
             msg = "Record successfully added"
@@ -50,6 +50,10 @@ def addrec():
 @app.route('/validate_user')
 def validate_user():
    return render_template('validate.html')
+
+@app.route('/update_user')
+def update_user():
+   return render_template('update.html')
 
 @app.route('/list')
 def list():
@@ -73,6 +77,47 @@ def validate():
 
       rows = cur.fetchall();
       return render_template("validate_result.html", rows=rows)
+
+@app.route('/search',methods = ['POST', 'GET'])
+def search():
+   if request.method == 'POST':
+      cws = request.form['cws']
+      con = sql.connect("database.db")
+      con.row_factory = sql.Row
+      cur = con.cursor()
+      cur.execute("select * from user_info where cws_id = (?)",(cws,))
+
+      rows = cur.fetchall();
+
+      return render_template("search_result.html", rows=rows)
+
+@app.route('/update',methods = ['POST', 'GET'])
+def update():
+   if request.method == 'POST':
+      try:
+         opt = operation.operation()
+         cws = request.form['cws']
+         phone_no = request.form['phone_no']
+         phone_model = request.form['phone_model']
+         created_time, ids = opt.calculate_code(cws, phone_no, phone_model)
+         with sql.connect("database.db") as con:
+            cur = con.cursor()
+
+            cur.execute("UPDATE user_info SET phone_no = ?,phone_model = ?,id = ?, updated_time = ? where cws_id = ? ",(phone_no, phone_model, ids, created_time, cws))
+
+            con.commit()
+            msg = "Record successfully updated"
+         if opt.qr_code_png(ids, cws):
+            msg = msg + " and QR code has been generated"
+            print("Sending mail to the user")
+            image_mail.Mailer().send_message(cws)
+      except Exception as err:
+         con.rollback()
+         msg = "error in update operation "+str(err)
+
+      finally:
+         return render_template("result.html", msg=msg)
+         con.close()
 
 
 if __name__ == '__main__':
--- database_table.py
+++ database_table.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('database.db')
 print ("Opened database successfully")
 
-conn.execute('CREATE TABLE user_info (cws_id TEXT, phone_no TEXT, phone_model TEXT,created_time DATE, id TEXT)')
+conn.execute('CREATE TABLE user_info (cws_id TEXT, phone_no TEXT, phone_model TEXT,created_time DATE, id TEXT, updated_time DATE)')
 print ("Table created successfully")
 conn.close()
